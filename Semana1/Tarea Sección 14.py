3cb1409023a7883319fa11debc0278619d147ca5
Mariana <marianallove2019gmail.com>
2025-01-25 16:10:57 -0600

    actividad salón.py

---
+++ Semana1/Tarea Sección 14.py
@@ -0,0 +1,53 @@
+print("Act 3: Ejercicios Capitulo 1 (tarea)")
+
+#Ejercicio 1 
+print("¿Cúal es la función de la memoria secundaria en una computadora?")
+print("Respuesta= Almacenar la información incluso cuando el equipo esta apagado")
+
+#Ejercicio 2 
+print("¿Qué es un programa?")
+print("Una serie de pasos escritos en un lenguaje de programación que le indican a la computadora que hacer")
+
+#Ejercicio 3
+print("¿Cúal es la diferencia entre un compilador y un intérprete")
+print("Un intérprete lee el código fuente de los programas tal y como ha sido escrito por el programador, lo analiza, e interpreta sus instrucciones sobre la marcha y Un compilador necesita que le entreguen el programa completo en un fichero, y luego ejecuta un proceso para traducir el código fuente de alto nivel a código máquina")
+
+#Ejercicio 4
+print("¿Cúal de los siguientes contiene código de máquina")
+print("Respuesta=El código fuente de phyton")
+
+
+#Ejercicio 5
+print("¿Qué esta mal en el siguiente código?")
+'''>>> primt '¡Hola, mundo!'
+File "<stdin>", line 1
+primt '¡Hola, mundo!'
+ˆ
+SyntaxError: invalid syntax
+>>>'''
+print("La palabra 'primt' es print")
+
+#Ejercicio 6
+print("¿En qué lugar del computador queda almacenada una variable, como en este caso “X”, después de ejecutar la siguiente línea de Python?: x = 123")
+print("En la memoria principal")
+
+#Ejercicio 7
+print("¿Qué mostrará en pantalla el siguiente programa?")
+'''x = 43
+x = x + 1
+print(x)'''
+print( "va a mostrar que x=44")
+
+#Ejercicio 8
+print("Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana") 
+print("Unidad central de procesamiento: El cerebro") 
+print("Memoria principal: Memoria a Corto Plazo")
+print("Memoria Secundaria: Memoria a corto Plazo") 
+print("Dispositivos de entrada: La vista y el oído")
+print("Dispositivos de salida: El habla o la escritura")
+
+#Ejercicio 9
+print("¿Cómo puedes corregir un “Error de sintaxis”?")
+print("Leyendo el código, cambiar el código y volviendo a ejecutar el programa, pensar en que estas haciendo mal o retroceder con los últimos pasos del código hasta dar con la solución")
+
+print("Fin de la tarea :)")
---
+++ Semana1/actividad salón.py
@@ -0,0 +1,13 @@
+print("ACTIVIDAD SALÓN")
+#ACTIVIDAD 1:
+print("HOLA MUNDO")
+
+#ACTIVIDAD 2:
+a= int(input("ingrese el primer número:"))
+b= int(input("ingrese el segundo número:"))
+print("suma=", a+b)
+print("resta=", a-b)
+print("multiplicación=", a*b)
+print("división=", a/b)
+
+#ACTIVIDAD 3:
