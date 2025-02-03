f7b53928acbfb7508f08d754882e167c80bf8c98
Mariana <marianallove2019gmail.com>
2025-02-03 17:53:35 -0600

    temporal

--- SEMANA 2/100 ejercicios python/Ejercicio 9.py
+++ SEMANA 2/100 ejercicios python/Ejercicio 9.py
@@ -1,19 +1,12 @@
 #ACTIVIDAD 9: Genera una lista de numeros e impares hasta un limite dado
 limit=int(input("ingrese un límite: "))
 par=[]
-impar=list()
+impar=[]
 
-for num in range (1,limit+1):
-    if num%2==0:
-        par.append(num)
+for i in range (1,limit+1):
+    if i%2==0:
+        par.append(i)
     else:
-        impar.append
-
-tamanio= max(len(par), len(impar))
-print(tamanio)
-
-for item in range(tamanio):
-    try:
-        print(par[item], impar[item], sep= '|')
-    except IndexError:
-        print('-', impar[item], sep= '|')
+        impar.append(i)
+print("numeros pares:", par)
+print("numeros impares:", impar)
