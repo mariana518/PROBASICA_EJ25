495d46d06dace5035b994b2f43d9644f1e3d48ee
Mariana PC <marianallove2019@gmail.com>
2025-05-19 09:31:02 -0600

    so

---
+++ SEMANA 2/100 ejercicios python/EJE GLOBAL.py
@@ -0,0 +1,4 @@
+estudiantes = [{"nombre": "Ana","matricula": "A001","carrera": "ITI","materias": [{"nombre": "Programación", "calificación": 90},{"nombre": "Matemáticas", "calificación": 85}]},
+    {"nombre": "Luis","matricula": "A002","carrera": "ITS","materias": [{"nombre": "Bases de Datos", "calificación": 95},{"nombre": "Cálculo", "calificación": 88}]}]
+
+print(estudiantes)
